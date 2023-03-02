72cfc39c6795fe2188f50af00168cd54be9266c1
Savyashree <savyashree.a@juegostudioz.com>
2023-03-02 17:40:24 +0530

    Added color code logic to all alliance

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function updateMainScreenData() {
 async function getElectionData() {
     $.getJSON("./data.json", function (data) {
         election_data = data;
-
         createAllianceDropdown()
     });
 }
@@ -35,6 +34,9 @@ function createAllianceDropdown() {
 
 
     var select = document.createElement("select");
+    select.name = "pets";
+    select.id = "pets"
+
     for (const val of uniqueValues) {
         var option = document.createElement("option");
         option.value = val;
@@ -42,6 +44,8 @@ function createAllianceDropdown() {
         select.appendChild(option);
     }
 
+    var label = document.createElement("label");
+    label.htmlFor = "pets";
 
     document.getElementById("alliance_container").appendChild(label).appendChild(select);
     // add an event listener to the select element
@@ -58,32 +62,51 @@ function createAllianceDropdown() {
 }
 
 function updateView(itemName) {
-    if (itemName == "alliance") {
-        itemName
-    }
+    // var district_names = [...new Set(election_data.filter((item) => { return (item.field15 === itemName && item.field14 == 1) }).map(item => { console.log(item.field15); return item.field6.toLowerCase() }))]
+    var election_winner_data = [...new Set(election_data.filter((item) => { return (item.field14 == 1) }).map(item => { console.log(item.field15); return { alliance: item.field15, district_name: item.field6.toLowerCase() } }))]
 
-    var color;
-    switch (itemName) {
-        case "NDA": color = "orange";
-            break
-        case "UPA": color = "#47acd1"
-            break;
-        case "Others": color = "#839195"
-            break;
-    }
-
-    var district_names = [...new Set(election_data.filter((item) => { return (item.field15 === itemName && item.field14 == 1) }).map(item => { console.log(item.field15); return item.field6.toLowerCase() }))]
-    console.log(election_data)
-
+    // console.log(election_winner_data)
     d3.select('#map')
         .selectAll('path')
         .attr('fill', function (d) {
-            if (district_names.includes(d.properties.NAME_2.toLowerCase())) {
-                return color; // Set the fill color to green
-            } else {
-                return 'lightblue'; // Set the fill color to red
-            }
-        })
+            let alliance = ""
 
+            if (election_winner_data.some(obj => obj.district_name === d.properties.NAME_2.toLowerCase())) {
+                election_winner_data.some(obj => {
+                    if (obj.district_name === d.properties.NAME_2.toLowerCase()) {
+                        alliance = obj.alliance;
+                        return true;
+                    }
+                });
+                return getColorCode(alliance);
+            }
+            else {
+                return 'lightblue'
+            }
+
+
+
+        })
+}
+
+function getColorCode(itemName) {
+    var color;
+    switch (itemName) {
+        case "NDA": color = "orange";
+            //var district_names = [...new Set(election_data.filter((item) => { return (item.field15 === itemName && item.field14 == 1) }).map(item => { console.log(item.field15); return item.field6.toLowerCase() }))]
+            break
+        case "UPA": color = "#47acd1"
+            //var district_names = [...new Set(election_data.filter((item) => { return (item.field15 === itemName && item.field14 == 1) }).map(item => { console.log(item.field15); return item.field6.toLowerCase() }))]
+            break;
+        case "Others": color = "#839195"
+            //var district_names = [...new Set(election_data.filter((item) => { return (item.field15 === itemName && item.field14 == 1) }).map(item => { console.log(item.field15); return item.field6.toLowerCase() }))]
+            break;
+        default: color = "purple"
+            //district_names = new Set(election_data.filter((item) => { return (item.field14 == 1) }).map(item => { return item.field6.toLowerCase() }))
+            break
+
+    }
+    console.log(typeof color)
+    return color
 
 }
